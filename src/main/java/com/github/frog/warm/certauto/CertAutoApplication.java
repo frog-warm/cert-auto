95d0b3cdbc78b583a023ac6810d49b1acce16ad9
tuzy <tuzy.cq@chinaccs.cn>
2024-12-11 09:40:22 +0800

    feat(a): init

---
+++ src/main/java/com/github/frog/warm/certauto/CertAutoApplication.java
@@ -0,0 +1,13 @@
+package com.github.frog.warm.certauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/frog/warm/certauto/CertAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.frog.warm.certauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
